6192d47c749399b778cea2bde2bed4dcf6010d74
Rade58 <bajic.rade2@gmail.com>
2022-12-18 10:32:15 +0100

    🦉

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,26 @@ import { createRoot } from "react-dom/client";
 import Pet from "./Pet";
 import Pets from "./Pets";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Adopt Some Pets!</h1>
-      <Pets />
-    </div>
-  );
+const container = document.getElementById("root");
 
-  /* return React.createElement("div", {}, [
+if (container) {
+  const App = () => {
+    return (
+      <div>
+        <h1>Adopt Some Pets!</h1>
+        <Pets />
+      </div>
+    );
+
+    /* return React.createElement("div", {}, [
     React.createElement("h1", { key: "main heading" }, "Adopt Some Pets!"),
     
   ]); */
-};
+  };
 
-const container = document.getElementById("root");
-// const root = ReactDOM.createRoot(container);
+  // const root = ReactDOM.createRoot(container);
 
-if (container) {
   const root = createRoot(container);
-  root.render(createElement(App));
+  // root.render(createElement(App));
+  root.render(<App />);
 }
