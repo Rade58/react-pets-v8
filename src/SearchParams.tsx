eda7dc32b636ef75f6b7cca02cd4ea3c3739e5a5
Rade58 <bajic.rade2@gmail.com>
2022-12-18 10:52:50 +0100

    🦉

---
+++ src/SearchParams.tsx
@@ -0,0 +1,11 @@
+import type { FC, ReactNode } from "react";
+
+interface Props {
+  children?: ReactNode;
+}
+
+const SearchParams: FC<Props> = () => {
+  return null;
+};
+
+export default SearchParams;
