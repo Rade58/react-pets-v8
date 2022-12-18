97ea7b8d7db874933d4cedf7e3e8e80062308414
Rade58 <bajic.rade2@gmail.com>
2022-12-18 08:37:55 +0100

    🦉

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import ReactDOM from "react-dom";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
 
 const Pet = ({ name, specie, breed }) => {
   return React.createElement("div", {}, [
