633542f9db7424c479a05f6cd8a839267abaf253
Rade58 <bajic.rade2@gmail.com>
2022-12-18 05:40:20 +0100

    🦉

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+const Pet = ({ name, specie, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { key: "name" }, name),
+    React.createElement("h2", { key: "specie" }, specie),
+    React.createElement("h3", { key: "breed" }, breed),
+  ]);
+};
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Some Pets!")
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", { key: "main heading" }, "Adopt Some Pets!"),
+    React.createElement(Pet, {
+      name: "Nakamaoto",
+      specie: "dog",
+      breed: "Shiba",
+      key: "nakamoto",
+    }),
+    React.createElement(Pet, {
+      name: "Kevin",
+      specie: "dog",
+      breed: "Havanese",
+      key: "kevin",
+    }),
+    React.createElement(Pet, {
+      name: "Pauly",
+      specie: "bird",
+      breed: "Parrot",
+      key: "pauly",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
