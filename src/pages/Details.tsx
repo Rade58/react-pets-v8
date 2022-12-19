f407b0d8900fe3c3d1c747c9522bc9fbccee992d
Rade58 <bajic.rade2@gmail.com>
2022-12-19 10:00:49 +0100

    🦉

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,13 +1,16 @@
 /* eslint jsx-a11y/anchor-is-valid: 1 */
 import React from "react";
 import type { FC, ReactNode } from "react";
+import { useParams } from "react-router-dom";
 
 interface Props {
   children?: ReactNode;
 }
 
 const Details: FC<Props> = () => {
-  return <div>Hi!</div>;
+  const { id } = useParams<"id">();
+
+  return <div>Hi! {id}</div>;
 };
 
 export default Details;
